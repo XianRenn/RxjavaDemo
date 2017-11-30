16be5183f23eb9e08fef8a334501cc2452a827e8
xianren <2517230090@qq.com>
2017-11-30 20:10:51 +0800

    being perfected

--- app/src/main/java/com/xianren/rxjavademo/MainActivity.java
+++ app/src/main/java/com/xianren/rxjavademo/MainActivity.java
@@ -1,7 +1,13 @@
 package com.xianren.rxjavademo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import rx.Observable;
+import rx.Observer;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void test(View view) {
+//       创建事件源
+        Student student = new Student();
+        student.name = "xxxxx";
+        Observer observer = new Observer() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onNext(Object o) {
+
+            }
+        };
+        Observable observable = Observable.just(student).map(new Func1() {
+            @Override
+            public Object call(Object o) {
+                Student student1 = (Student) o;
+                String str = student1.name;
+                return null;
+            }
+        }).subscribeOn(Schedulers.io());
+        observable.subscribe(observer);
+
+
     }
 }
---
+++ app/src/main/java/com/xianren/rxjavademo/Student.java
@@ -0,0 +1,9 @@
+package com.xianren.rxjavademo;
+
+/**
+ * Created by xianren on 2017/11/30.
+ */
+
+public class Student {
+    public  String name;
+}
