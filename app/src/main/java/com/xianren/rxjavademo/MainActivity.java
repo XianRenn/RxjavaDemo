edbaaecab96866c415d9c6f8e5019959cbf4f645
xianren <2517230090@qq.com>
2017-11-30 21:23:04 +0800

    being perfected

--- app/src/main/java/com/xianren/rxjavademo/MainActivity.java
+++ app/src/main/java/com/xianren/rxjavademo/MainActivity.java
@@ -2,10 +2,12 @@ package com.xianren.rxjavademo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import rx.Observable;
 import rx.Observer;
+import rx.Subscriber;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
 //       创建事件源
         Student student = new Student();
         student.name = "xxxxx";
+        Subscriber<Student> obj = null;
+
         Observer observer = new Observer() {
             @Override
             public void onCompleted() {
@@ -37,15 +41,31 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
-        Observable observable = Observable.just(student).map(new Func1() {
+        Observable.just(student).map(new Func1() {
             @Override
             public Object call(Object o) {
                 Student student1 = (Student) o;
                 String str = student1.name;
+                Log.i("flag", "jhfj");
                 return null;
             }
-        }).subscribeOn(Schedulers.io());
-        observable.subscribe(observer);
+        }).subscribeOn(Schedulers.io()).subscribe(new Subscriber(obj) {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onNext(Object o) {
+                Student obj = (Student) o;
+                Log.i("flag", "jhfj");
+            }
+        });
 
 
     }
