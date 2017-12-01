66a105da73bb81f65c4959c052f6fa33f9cd4279
xianren <2517230090@qq.com>
2017-12-01 10:23:38 +0800

    being perfected

--- app/src/main/java/com/xianren/rxjavademo/MainActivity.java
+++ app/src/main/java/com/xianren/rxjavademo/MainActivity.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import android.view.View;
 
 import rx.Observable;
-import rx.Observer;
 import rx.Subscriber;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
@@ -20,14 +19,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void test(View view) {
-//       创建事件源
-        Student student = new Student();
-        student.name = "xxxxx";
-        Subscriber<Student> obj = null;
-        Observable.just(student).map(new Func1() {
+        getObservalbe().map(new Func1() {
             @Override
             public Object call(Object o) {
-//                接收just传递的数据
+//                可以接收事件源过来传递的数据
                 Student student1 = (Student) o;
                 String str = student1.name;
                 Log.i("flag", "jhfj");
@@ -52,6 +47,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+
+    public Observable<Student> getObservalbe() {
+        Student student = new Student();
+        student.name = "yyyyyyy";
+//       设置事件源
+        Observable<Student> obj = Observable.just(student);
+        return obj;
 
     }
 }
