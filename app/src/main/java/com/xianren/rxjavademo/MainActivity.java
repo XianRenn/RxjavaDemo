0c29da093ec427d53febcaa0e3dd148951d4e520
xianren <2517230090@qq.com>
2017-12-01 11:55:53 +0800

    泛型和Rx java结合使用

--- app/src/main/java/com/xianren/rxjavademo/MainActivity.java
+++ app/src/main/java/com/xianren/rxjavademo/MainActivity.java
@@ -6,57 +6,68 @@ import android.util.Log;
 import android.view.View;
 
 import rx.Observable;
-import rx.Subscriber;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+    Result<Student> result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void test(View view) {
-        getObservalbe().map(new Func1() {
+        result = new Result<Student>() {
             @Override
-            public Object call(Object o) {
-//                可以接收事件源过来传递的数据
-                Student student1 = (Student) o;
-                String str = student1.name;
-                Log.i("flag", "jhfj");
-                return str;
+            void succeed(Student data) {
+                Log.i("stu", data.name);
             }
-        }).subscribeOn(Schedulers.io()).subscribe(new Subscriber() {
+
             @Override
             public void onCompleted() {
-
+                Log.i("stu", "stu");
             }
 
             @Override
             public void onError(Throwable throwable) {
-
             }
 
             @Override
-            public void onNext(Object o) {
-//map转换数据类型，接收call返回的数据
-                Student obj = (Student) o;
-                Log.i("flag", "jhfj");
+            public void onNext(Student student) {
+                Log.i("stu", student.name);
             }
-        });
+        };
+    }
+
+     public void test(View view) {
+        getData(Student.class).map(new Func1() {
+            @Override
+            public Object call(Object o) {
+//                可以接收事件源过来传递的数据
+                Student stuObj = (Student) o;
+                return stuObj;
+            }
+        }).subscribeOn(Schedulers.io()).subscribe(result);
 
 
     }
 
-
-    public Observable<Student> getObservalbe() {
-        Student student = new Student();
-        student.name = "yyyyyyy";
+    public Observable<?> getData(Class T) {
+        Student student;
+        Teacher teacher;
+        Object tem;
+        if (T == Student.class) {
+            student = new Student();
+            student.name = "I am a student";
+            tem = student;
+        } else {
+            teacher = new Teacher();
+            teacher.name = "I am a teacher";
+            tem = teacher;
+        }
 //       设置事件源
-        Observable<Student> obj = Observable.just(student);
+        Observable<?> obj = Observable.just(tem);
         return obj;
-
     }
+
+
 }
