245e54c6354561f30699b45ead9898fb10d1663c
XianRenn <2517230090@qq.com>
2017-12-24 18:10:33 +0800

    being perfect

--- app/src/main/java/com/xianren/rxjavademo/MainActivity.java
+++ app/src/main/java/com/xianren/rxjavademo/MainActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import rx.Observable;
-import rx.Observer;
 import rx.Subscriber;
-import rx.functions.Func1;
-import rx.schedulers.Schedulers;
+import rx.android.schedulers.AndroidSchedulers;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,17 +23,7 @@ public class MainActivity extends AppCompatActivity {
 //       创建事件源
         Student student = new Student();
         student.name = "xxxxx";
-        Subscriber<Student> obj = null;
-        Observable.just(student).map(new Func1() {
-            @Override
-            public Object call(Object o) {
-//                接收just传递的数据
-                Student student1 = (Student) o;
-                String str = student1.name;
-                Log.i("flag", "jhfj");
-                return str;
-            }
-        }).subscribeOn(Schedulers.io()).subscribe(new Subscriber() {
+        Observable.just(student).subscribeOn(AndroidSchedulers.mainThread()).subscribe(new Subscriber() {
             @Override
             public void onCompleted() {
 
@@ -49,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
 //map转换数据类型，接收call返回的数据
                 Student obj = (Student) o;
                 Log.i("flag", "jhfj");
+                Toast.makeText(MainActivity.this, "fds", Toast.LENGTH_SHORT).show();
+
             }
         });
 
